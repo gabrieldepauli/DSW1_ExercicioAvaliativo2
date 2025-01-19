94472c801f10a06cf5118bc2e7cae10f6e6f2790
gabrieldepauli <gabrieldepauli.ads@gmail.com>
2025-01-19 18:10:53 -0300

    Correção no método de mostrar mensagem caso a lista de pedidos esteja vazia

--- ExercicioAvaliativo/src/main/java/controller/command/ListPedidosCommand.java
+++ ExercicioAvaliativo/src/main/java/controller/command/ListPedidosCommand.java
@@ -18,6 +18,8 @@ public class ListPedidosCommand implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		String message = "";
+		
 		var user = (User) request.getSession(false).getAttribute("user_id");
 		
 		PedidoDao dao = new PedidoDaoFactory().factory();
@@ -25,6 +27,12 @@ public class ListPedidosCommand implements Command {
 		List<Pedido> pedidos = dao.retrive(user);
 		
 		request.setAttribute("pedidos", pedidos);
+		
+		if(pedidos.isEmpty()) {
+			message = "Não há pedidos registrados.";
+			
+			request.setAttribute("message", message);
+		}
 		
 		// Mensagem para o console para ver se os pedidos estão sendo passados
 		System.out.println("Pedidos: " + pedidos);
